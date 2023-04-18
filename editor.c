492648960f2cf4271c98f56d9a804ab3d8589430
illegitimate-egg <cameron@humaneyestudio.co.uk>
2023-04-18 13:57:20 +0100

    Message

--- editor.c
+++ editor.c
@@ -10,6 +10,10 @@ void draw_main_window(FILE* data, size_t length) {
 	local_win = create_newwin(window_height + 2, window_width + 2, 0, 22, true);
 	local_win_content = create_newwin(window_height, window_width, 1, 23, false);
 
+	wmove(local_win, 0, 1);
+	wprintw(local_win, "Viewer:");
+	wrefresh(local_win);
+
 	unsigned int block = 0;
 	for (int i = 0; i < length; i++) {
 		fread(&block, 1, 1, data);
